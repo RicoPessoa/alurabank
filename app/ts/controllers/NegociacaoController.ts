c9cfbd53d87049d043b5ee804a4ef85e6828cd7c
Ricardo Pessoa <ricardoandre.pessoa@gmail.com>
2020-10-22 13:22:39 -0300

    index

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,7 +1,5 @@
-import { NegociacoesView } from '../views/NegociacoesView'; 
-import { MensagemView } from '../views/MensagemView'; 
-import { Negociacoes } from '../models/Negociacoes'; 
-import { Negociacao } from '../models/Negociacao'; 
+import { NegociacoesView, MensagemView } from '../views/index';
+import { Negociacao, Negociacoes } from '../models/index';
 
 export class NegociacaoController {
 
@@ -10,10 +8,9 @@ export class NegociacaoController {
     private _inputValor: JQuery;
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
-    private _mesagemView = new MensagemView('#mensagemView')
-
+    private _mensagemView = new MensagemView('#mensagemView');
+    
     constructor() {
-
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
@@ -25,14 +22,14 @@ export class NegociacaoController {
         event.preventDefault();
 
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g, ',')),
+            new Date(this._inputData.val().replace(/-/g, ',')), 
             parseInt(this._inputQuantidade.val()),
-            parseInt(this._inputValor.val())
+            parseFloat(this._inputValor.val())
         );
 
         this._negociacoes.adiciona(negociacao);
-        this._negociacoesView.update(this._negociacoes);
-        this._mesagemView.update('Negociação adicionada com sucesso')
-    }
 
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update('Negociação adicionada com sucesso!');
+    }
 }
